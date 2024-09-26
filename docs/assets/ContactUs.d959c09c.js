4c1fbb27c6766ed92f77eccf7401f5c832e5a299
Li <1542902425.@QQ.com>
2024-09-26 15:44:52 +0800

    vite配置，标签页修复

--- docs/assets/ContactUs.d959c09c.js
+++
@@ -1,1 +0,0 @@
-import{_ as n,c as s,l as t,o}from"./index.487d67f6.js";const a={},c={id:"ContactUs"};function _(r,e){return o(),s("div",c,e[0]||(e[0]=[t("div",{class:"banner container-fuild text-center"},"\u8054\u7CFB\u6211\u4EEC",-1),t("div",null," 123 ",-1)]))}const f=n(a,[["render",_],["__scopeId","data-v-af8912f9"]]);export{f as default};
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  
   base: './',
   plugins: [
     vue(),
@@ -32,6 +31,9 @@ export default defineConfig({
     alias: {
       '@': resolve(__dirname, './src')
     }
+  },
+  build: {
+    outDir: 'docs', // 指定输出路径
   }
 })
 
