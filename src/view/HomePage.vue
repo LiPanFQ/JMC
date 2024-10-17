72877ec4fccd8ab2700c38d5d9f33c89f24199be
Li <1542902425.@QQ.com>
2024-10-17 10:11:17 +0800

    迭代2，持续优化

--- src/view/HomePage.vue
+++ src/view/HomePage.vue
@@ -1,9 +1,11 @@
 <template>
   <FiveMetalFactory />
+  <!-- <demo/> -->
 </template>
 
 <script setup name="HomePage">
 import FiveMetalFactory from '@/components/FiveMetalFactory.vue'
+import demo from '@/components/demo.vue'
 </script>
 
 <style scoped>
